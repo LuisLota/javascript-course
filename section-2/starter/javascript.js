dc39aadb176b0f6bfe112900278711c8009b1142
LuisLota <louis.miguel.05@gmail.com>
2019-05-08 18:15:21 +0100

    code challenge 1

--- section-2/starter/javascript.js
+++ section-2/starter/javascript.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+// coding challenge 1 //
+
+// BMI(Body mass index) 
+
+let name = prompt('what is your name ?');
+let checked;
+let height = 1.83;
+let mass = 75;
+
+let bmi = mass / (height * height);
+console.log('bmi -->',bmi);
+
+
+if(name === 'john'){
+    checked = true;
+    console.log('john bmi is: ',bmi);
+    if(checked == true){
+        window.alert('is ' + name + ' highter then mark? ' + checked)
+    }
+
+}
+else {
+    checked = false;
+    console.log('makr bmi is: ',bmi);
+    window.alert('is ' + name + ' highter then mark? ' + checked)
+}
+
+
