282bc531e81a51fa9414f8b1cea1d707a4066106
LuisLota <louis.miguel.05@gmail.com>
2019-06-16 17:25:04 +0100

    shifts, textContent and use toggle for animations

--- section-4/pig-game/app.js
+++ section-4/pig-game/app.js
@@ -33,5 +33,20 @@ let diceDOM = document.querySelector('.dice');
 diceDOM.style.display = 'block';
 diceDOM.src = 'dice-' + dice + '.png';
 
-// 3.Update the round socre if the rolled number was not a 1 
+// 3.Update the round score if the rolled number was not a 1 
+if(dice !== 1){
+    roundScore +=dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+} else {
+     //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+
+   
+}
 });
